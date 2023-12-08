7bcd6121b4f7d28188a955da384c4f28b6f6d134
iferc <git@iferc.ca>
2023-12-08 00:02:52 -0500

    Add some colour themes with Canadian flourish, containing placeholder configuration options to explore later.

---
+++ after/plugin/colours.lua
@@ -0,0 +1,116 @@
+function ColorThemeSet(color)
+    -- kanagawa-dragon, sherbet, nordic
+	color = color or "kanagawa"
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+-- Default options:
+require('kanagawa').setup({
+    compile = false,             -- enable compiling the colorscheme
+    undercurl = true,            -- enable undercurls
+    commentStyle = { italic = true },
+    functionStyle = {},
+    keywordStyle = { italic = true},
+    statementStyle = { bold = true },
+    typeStyle = {},
+    transparent = false,         -- do not set background color
+    dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
+    terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+    colors = {                   -- add/modify theme and palette colors
+        palette = {},
+        theme = {
+            -- change specific usages for a certain theme, or for all of them
+            -- wave = {
+            --     ui = {
+            --         float = {
+            --             bg = "none",
+            --         },
+            --     },
+            -- },
+            -- dragon = {
+            --     syn = {
+            --         parameter = "yellow",
+            --     },
+            -- },
+            -- all = {
+            --     ui = {
+            --         bg_gutter = "none"
+            --     }
+            -- }
+        },
+    },
+    overrides = function(colors) -- add/modify highlights
+        local theme = colors.theme
+        return {
+            NormalFloat = { bg = "none" },
+            FloatBorder = { bg = "none" },
+            FloatTitle = { bg = "none" },
+
+            -- Save an hlgroup with dark background and dimmed foreground
+            -- so that you can use it where your still want darker windows.
+            -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+            NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+
+            -- Popular plugins that open floats will link to NormalFloat by default;
+            -- set their background accordingly if you wish to keep them dark and borderless
+            LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+            MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+        }
+    end,
+    theme = "wave",              -- Load "wave" theme when 'background' option is not set
+    background = {               -- map the value of 'background' option to a theme
+        dark = "wave",           -- try "dragon" !
+        light = "lotus"
+    },
+})
+
+require 'nordic' .setup {
+    -- This callback can be used to override the colors used in the palette.
+    on_palette = function(palette) return palette end,
+    -- Enable bold keywords.
+    bold_keywords = false,
+    -- Enable italic comments.
+    italic_comments = true,
+    -- Enable general editor background transparency.
+    transparent_bg = false,
+    -- Enable brighter float border.
+    bright_border = false,
+    -- Reduce the overall amount of blue in the theme (diverges from base Nord).
+    reduced_blue = true,
+    -- Swap the dark background with the normal one.
+    swap_backgrounds = false,
+    -- Override the styling of any highlight group.
+    override = {},
+    -- Cursorline options.  Also includes visual/selection.
+    cursorline = {
+        -- Bold font in cursorline.
+        bold = false,
+        -- Bold cursorline number.
+        bold_number = true,
+        -- Avialable styles: 'dark', 'light'.
+        theme = 'dark',
+        -- Blending the cursorline bg with the buffer bg.
+        blend = 0.7,
+    },
+    noice = {
+        -- Available styles: `classic`, `flat`.
+        style = 'flat',
+    },
+    telescope = {
+        -- Available styles: `classic`, `flat`.
+        style = 'flat',
+    },
+    leap = {
+        -- Dims the backdrop when using leap.
+        dim_backdrop = false,
+    },
+    ts_context = {
+        -- Enables dark background for treesitter-context window
+        dark_background = true,
+    }
+}
+
+ColorThemeSet()
