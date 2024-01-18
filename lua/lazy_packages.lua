7359632301f85ef398e43b8beb0917bef6de3726
iferc <git@iferc.ca>
2024-01-17 23:23:06 -0500

    Add neo-tree file tree browser, and enable hijacking of netrw.

--- lua/lazy_packages.lua
+++ lua/lazy_packages.lua
@@ -309,6 +309,22 @@ require("lazy").setup({
     },
 
     {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+            "3rd/image.nvim",              -- Optional image support in preview window: See `# Preview Mode` for more information
+        },
+        opts = {
+            filesystem = {
+                hijack_netrw_behavior = "open_default",
+            }
+        },
+    },
+
+    {
         'theprimeagen/harpoon',
         branch = 'harpoon2',
         dependencies = { { 'nvim-lua/plenary.nvim' } }
