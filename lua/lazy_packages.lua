9cc39380fde8104310e6ca76966b26ab7980140b
Jenifer Champagne <jen@iferc.ca>
2024-04-04 01:55:07 -0400

    Improve tree-sitter context setup to be concise and not cursor-based.

--- lua/lazy_packages.lua
+++ lua/lazy_packages.lua
@@ -119,8 +119,25 @@ require("lazy").setup({
         end,
     },
 
-    -- todo: Unsure if this is needed. To confirm.
-    'nvim-treesitter/nvim-treesitter-context',
+    -- Shows context lines at top of buffer, such as seeing the
+    -- function name when it is too far away to be on the screen.
+    {
+        'nvim-treesitter/nvim-treesitter-context',
+        opts = {
+            enable = true,           -- Enable this plugin (Can be enabled/disabled later via commands)
+            max_lines = 8,           -- How many lines the window should span. Values <= 0 mean no limit.
+            min_window_height = 32,  -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+            line_numbers = true,
+            multiline_threshold = 1, -- Maximum number of lines to show for a single context, default: 20
+            trim_scope = 'outer',    -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+            mode = 'topline',        -- Line used to calculate context. Choices: 'cursor', 'topline'
+            -- Separator between context and content. Should be a single character string, like '-'.
+            -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+            separator = nil,
+            zindex = 20,     -- The Z-index of the context window
+            on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+        },
+    },
 
     -- Playground to visualize the Abstract Syntax Tree.
     'nvim-treesitter/playground',
