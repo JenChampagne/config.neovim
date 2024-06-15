aca8180ff3650e16a1da283bb43081ccafd5d648
Jenifer Champagne <jen@iferc.ca>
2024-06-14 20:16:12 -0400

    Create easy build script based on current build instructions.

---
+++ easy_build_install.sh
@@ -0,0 +1,28 @@
+git submodule update --init
+
+cd neovim
+
+logfile="../ebi_$(date -u +%FT%H-%M-%SZ)_"
+
+read -p "Update to latest commit of neovim? " confirm
+
+if [[ "$confirm" == [yY] || "$config" == [yY][eE][sS] ]]; then
+    echo "Pulling latest commit from GitHub."
+    git checkout -d origin/master
+fi
+
+echo "Cleaning past build files."
+make distclean >"${logfile}clean.log"
+
+echo "Building dependencies."
+make deps >"${logfile}dependencies.log"
+
+echo "Building application."
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    make CMAKE_BUILD_TYPE=RelWithDebInfo >"${logfile}make.log"
+else
+    make CMAKE_BUILD_TYPE=Release >"${logfile}make.log"
+fi
+
+echo "Install application for system-wide use."
+sudo make install >"${logfile}setup.log"
