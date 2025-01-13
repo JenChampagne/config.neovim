02ed419d7b0aca2420a91102ca433b24f299b1de
Jenifer Champagne <jen@iferc.ca>
2025-01-12 23:17:42 -0500

    deps: Upgrade dependencies. ui: Remove mode display in statuc line of every window.

--- lua/lazy_packages.lua
+++ lua/lazy_packages.lua
@@ -368,7 +368,8 @@ require("lazy").setup({
                     }
                 },
                 sections = {
-                    lualine_a = { 'mode' },
+                    -- lualine_a = { 'mode' },
+                    lualine_a = {},
                     lualine_b = { 'branch', 'diff', 'diagnostics' },
                     lualine_c = { { 'filename', path = 1 } },
                     -- lualine_x = { 'encoding', 'fileformat', 'filetype' },
